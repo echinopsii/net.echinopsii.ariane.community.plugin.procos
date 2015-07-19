41ce91e3780ffaf0794956401d610225d4f4005b
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-07-19 18:28:25 +0200

    [ACC-184] init procos plugin

---
+++ README.md
@@ -0,0 +1,6 @@
+ariane.community.plugin.procos
+===============================
+
+Ariane Community Plugin Procos. Map your operating system process and more thanks psutil.
+
+Documentation coming soon...
---
+++ setup.py
@@ -0,0 +1,43 @@
+# ***** BEGIN LICENSE BLOCK *****
+#
+# For copyright and licensing please refer to LICENSE.
+#
+# ***** END LICENSE BLOCK *****
+from setuptools import setup
+
+long_description = ('Ariane Plugin PROCOS map your operating process interactions and more.'
+                    'Where you can get more informations : '
+                    '   + http://ariane.echinopsii.net'
+                    '   + http://confluence.echinopsii.net/confluence/display/AD/Ariane+Documentation+Home'
+                    '   + IRC on freenode #ariane.echinopsii')
+
+setup(name='ariane_procos',
+      version='0.1.0-b01',
+      description='Ariane Plugin PROCOS',
+      long_description=long_description,
+      author='Mathilde Ffrench',
+      author_email='mathilde.ffrench@echinopsii.net',
+      maintainer='Mathilde Ffrench',
+      maintainer_email='mathilde.ffrench@echinopsii.net',
+      url='https://github.com/echinopsii/net.echinopsii.ariane.community.plugin.procos.git',
+      download_url='https://github.com/echinopsii/net.echinopsii.ariane.community.plugin.procos.git/tarball/0.1.0-b01',
+      packages=['ariane_procos'],
+      license='AGPLv3',
+      install_requires=['requests', 'ariane-clip3', 'pykka', 'python-daemon'],
+      package_data={'': ['LICENSE', 'README.md']},
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Communications',
+          'Topic :: Internet',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Monitoring',
+          'Topic :: System :: Networking'],
+      zip_safe=True)
