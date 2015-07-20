d1100ebb2ad8e1a1f51eb1392d4b702ea11f7481
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-07-20 21:46:05 +0200

    [ACC-184] add ariane_connector

---
+++ tests/acceptance/ariane_connector_at.py
@@ -0,0 +1,12 @@
+import unittest
+
+__author__ = 'mffrench'
+
+
+class ArianeConnectorTest(unittest.TestCase):
+
+    def test_start_and_stop_with_no_gears(self):
+        pass
+
+    def test_start_and_stop_with_one_gear(self):
+        pass
