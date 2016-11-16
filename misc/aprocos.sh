2b6ef15e6e6560d2421373badc46c1b7a10d7e2b
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-11-16 15:01:18 +0100

    [ACPPOS-40] manage freebsd os

--- misc/aprocos.sh
+++ misc/aprocos.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+OS=`uname`
+
+if [[ "$OS" = "FreeBSD" ]] || [[ "$OS" = "Linux" ]]; then
+	echo "You are using $OS : system supported."
+else
+	echo "You are using $OS : system not currently supported."
+	echo "Will use Linux behaviors... May not work correctly"
+	echo "Contributions are welcome ;)"
+fi
+
 case "$1" in
     start)
         if [ "${UID}" = "" ]; then
@@ -18,14 +28,26 @@ case "$1" in
         exec -a ariane-plg.procos python3 -m ariane_procos > /var/log/ariane/aprocos_nohup.log 2>&1 &
         ;;
     stop)
-        stopped=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' | wc -l`
+	if [ "$OS" = "FreeBSD" ]; then
+		stopped=`ps -a | grep 'ariane_procos' | grep -v grep | awk '{print $1}' | wc -l`
+	else
+	        stopped=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' | wc -l`
+	fi
         if [ $stopped -eq 1 ]; then
             printf "Stopping Ariane ProcOS "
-            pid=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' `
+	    if [ "$OS" = "FreeBSD" ]; then
+		    pid=`ps -a | grep 'ariane_procos' | grep -v grep | awk '{print $1}'`
+	    else
+	            pid=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' `
+	    fi
             kill $pid
 
             count=0
-            stopped=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' | wc -l`
+	    if [ "$OS" = "FreeBSD" ]; then
+		    stopped=`ps -a | grep 'ariane_procos' | grep -v grep | awk '{print $1}' | wc -l`
+	    else
+	            stopped=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' | wc -l`
+	    fi
             while [ $stopped -eq 1 ]
             do
                 count=`expr $count + 1`
@@ -34,7 +56,11 @@ case "$1" in
                 fi
                 sleep 1
                 printf "."
-                stopped=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' | wc -l`
+		if [ "$OS" = "FreeBSD" ]; then
+			stopped=`ps -a | grep 'ariane_procos' | grep -v grep | awk '{print $1}' | wc -l`
+		else
+	                stopped=`ps -aef | grep 'ariane_procos' | grep -v grep | awk '{print $2}' | wc -l`
+		fi
             done
             printf "\nAriane ProcOS is stopped.\n"
         else
